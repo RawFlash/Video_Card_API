87836f5afaeb2510aa005fba181e01f46e7acac8
RawFlash <vip.pahan999@gmail.com>
2019-10-27 21:36:38 +0700

    first bot

--- video_card_api/views.py
+++ video_card_api/views.py
@@ -5,6 +5,40 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import os
+import random
+import http.client
+import tor.tor
+import time
+
+user_agent_list = [
+   #Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    #Firefox
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+    'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+    'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+]
+
+url = 'https://video-card-api.herokuapp.com/test'
 
 def main2(request):
 
@@ -12,23 +46,29 @@ def main2(request):
 
 def bots(request):
 
-    http_proxy = "http://10.10.1.10:3128"
-    https_proxy = "https://10.10.1.11:1080"
-    ftp_proxy = "ftp://10.10.1.10:3128"
+    print(request)
 
-    proxyDict = {
-        "http": http_proxy,
-        "https": https_proxy,
-        "ftp": ftp_proxy
+    http_proxy = "http://194.62.145.248:8080"
+    https_proxy = "https://194.62.145.248:8080"
+    ftp_proxy = "10.10.1.10:3128"
+
+    proxiesD = {
+        "http": "http://194.62.145.248:8080",
+        "https": "https://194.62.145.248:8080"
     }
 
-    s = requests.get("https://video-card-api.herokuapp.com/test" )#,proxies = proxyDict)
-    i=0
-    print('!!')
-    while i<5:
 
-        i=i+1
-        s = requests.get("https://video-card-api.herokuapp.com/test") #,proxies=proxyDict)
+
+    print('!!')
+    for i in range(1, 1000):
+        user_agent = random.choice(user_agent_list)
+        headers = {'User-Agent': user_agent}
+
+        s= requests.get(url,headers = headers) #,proxies=proxiesD)
+        t = random.randint(0,200)
+        print('request: Number:'+ str(i) +', break:'+ str(t) )
+        time.sleep(t)
+
 
     return HttpResponse("Боты сработали")
 
